dd9ff833e83df720f16adda254aa7bd6e980716b
naveh-elya <naveh.elya@post.runi.ac.il>
2025-10-28 23:28:27 +0200

    fixed bug

--- NumWords.java
+++ NumWords.java
@@ -2,9 +2,9 @@
 public class NumWords {
 	public static void main(String args[]) {
 	    String number = args[0];
-		char hundreds = (args.length >=3) ? number.charAt(args.length - 3) : '0';
-		char tens = (args.length >= 2) ? number.charAt(args.length - 2) : '0';
-		char ones = (args.length >= 1) ? number.charAt(args.length - 1) : '0';
+		char hundreds = (number.length() >=3) ? number.charAt(number.length() - 3) : '0';
+		char tens = (number.length() >= 2) ? number.charAt(number.length() - 2) : '0';
+		char ones = (number.length() >= 1) ? number.charAt(number.length() - 1) : '0';
 
 
 		System.out.printf("%s hundreds, %s tens, and %s ones.", hundreds, tens, ones);
