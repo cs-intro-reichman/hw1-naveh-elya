af31919719db126a784aa4f04a41f93064f3d73e
naveh-elya <naveh.elya@post.runi.ac.il>
2025-10-28 22:12:33 +0200

    solved FVCalc

--- FVCalc.java
+++ FVCalc.java
@@ -1,6 +1,16 @@
 // Computes the future value of a saving investment.
 public class FVCalc {
 	public static void main(String[] args){
-		// Replace this comment with your code
+		int startMoney = Integer.parseInt(args[0]);
+		double ratio = Double.parseDouble(args[1]);
+		int years = Integer.parseInt(args[2]);
+
+		int countSaving = startMoney;
+
+		for (int i = 0; i < years; i++) {
+			countSaving += countSaving * (ratio / 100);	
+		}
+
+		System.out.printf("After %d years, $%d saved at %.1f25 will yield $%d", years, startMoney, ratio, countSaving);
 	}
 }
