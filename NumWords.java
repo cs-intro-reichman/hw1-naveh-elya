f2a948eac2bafb27fe67fb377d5017e150cc91fb
naveh-elya <naveh.elya@post.runi.ac.il>
2025-10-28 23:31:35 +0200

    fixed more then 3 digits number

--- NumWords.java
+++ NumWords.java
@@ -2,7 +2,7 @@
 public class NumWords {
 	public static void main(String args[]) {
 	    String number = args[0];
-		char hundreds = (number.length() >=3) ? number.charAt(number.length() - 3) : '0';
+		String hundreds = (number.length() >=3) ? number.substring(0, number.length() - 3) : "0";
 		char tens = (number.length() >= 2) ? number.charAt(number.length() - 2) : '0';
 		char ones = (number.length() >= 1) ? number.charAt(number.length() - 1) : '0';
 
