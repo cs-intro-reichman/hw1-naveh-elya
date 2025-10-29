bfd1bcac9a165af69b851d53978ae102f42be97a
naveh-elya <naveh.elya@post.runi.ac.il>
2025-10-29 18:50:32 +0200

    finished the ascend as wanted I hope

--- Ascend.java
+++ Ascend.java
@@ -1,24 +1,34 @@
 // Generates three integer random numbers in a given range,
 // and prints them in increasing order.
-import java.util.Arrays;
-import java.util.Random;
 
 public class Ascend {
 	public static void main(String[] args) {
 		// Replace this comment with your code
-		Random random = new Random();
 
-		int rangeStartString = Integer.parseInt(args[0]);
-		int rangeEndString = Integer.parseInt(args[1]);
+		int limit = Integer.parseInt(args[0]);
 
 		int[] numbers = new int[3];
 		for (int i = 0; i < 3; i++) {
-			numbers[i] = random.nextInt(rangeEndString - rangeStartString +1) + rangeStartString;
+			numbers[i] = (int) (Math.random() * limit);
 		}
-		Arrays.sort(numbers);
 
-		for (int j = 0; j < 10; j++) {
-			System.err.println(numbers[j]);
+		for (int j = 0; j < 3; j++) {
+			System.out.print(numbers[j] + (j < 2 ? " " : "\n"));
+		}
+		int first = numbers[0];
+		int second = numbers[1];
+		int third = numbers[2];
+
+		int min = Math.min(Math.min(first, second), third);
+		int max = Math.max(Math.max(first, second), third);
+		int middle = (first + second + third) - (min + max);
+
+		numbers[0] = min;
+		numbers[1] = middle;
+		numbers[2] = max;
+
+		for (int j = 0; j < 3; j++) {
+			System.out.print(numbers[j] + (j < 2 ? " " : ""));
 		}
 	}
 }
